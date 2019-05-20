55405a3a1a4247d44bf0321505efdaa1d87b7e5f
root <jd.cristof93@gmail.com>
2019-05-19 21:33:38 -0300

    commit inicial

---
+++ components/footer/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="footer">
+    <!-- <div class="columns container">
+      <div class="column">
+        <p>Tudo em Pneus você encontra aqui!.</p>
+      </div> -->
+    <div class="column has-text-right">
+      <h2 class="subtitle">
+          PneuCommerce<br>
+          Tudo em Pneus Você encontra aqui!!<br>
+          Conteudo Produzido por João Dutra Cristoforu<br>
+          2019
+      </h2>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VmFooter',
+}
+</script>
+
+<style lang="scss" scoped>
+ .footer {
+   background: rgb(11, 17, 65);
+   color: black;
+   
+   
+ }
+  h2{
+    color: white;
+    text-align: center;
+  }
+</style>
---
+++ components/hero/Hero.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hero is-medium is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          PneuCommerce
+        </h1>
+        <h2 class="subtitle">
+          Tudo em Pneus, você encontra aqui!
+        </h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+    .hero {
+      background: rgb(11, 17, 65);
+      
+    }
+    h1, h2 {
+      color: #fff;
+    }
+    // .hero-body{
+    //   height: 100px;
+    // }
+    
+</style>
